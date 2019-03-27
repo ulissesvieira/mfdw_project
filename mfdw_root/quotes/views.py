618575342af6b480665ec032a79fd6be672fd153
Ulisses Vieira <sr.jvieira03@gmail.com>
2019-03-26 21:05:17 -0300

    Create of a ListView and a DetailView

--- mfdw_root/quotes/views.py
+++ mfdw_root/quotes/views.py
@@ -1,9 +1,36 @@
 from django.apps import apps
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
+from .models import Quote
 from .forms import QuoteForm
 # from pages.models import Page # did not work
+
+
+class QuoteList(ListView):
+    model = Quote
+    context_object_name = 'all_quotes'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        Page = apps.get_model('pages', 'Page')
+
+        context = super(QuoteList, self).get_context_data(**kwargs)
+        context['page_list'] = Page.objects.all()
+        return context
+
+
+class QuoteView(DetailView):
+    model = Quote
+    context_object_name = 'quote'
+
+    def get_context_data(self, **kwargs):
+        Page = apps.get_model('pages', 'Page')
+
+        context = super(QuoteView, self).get_context_data(**kwargs)
+        context['page_list'] = Page.objects.all()
+        return context
 
 
 def quote_req(request):
